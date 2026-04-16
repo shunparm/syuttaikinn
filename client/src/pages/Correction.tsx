507da59b02cb066a8433fbdee070113ff6b77a5e
shunparm <shunparm@gmail.com>
2026-04-16 09:41:39 +0900

    fix: Correction.tsxの変数重複宣言を修正

--- client/src/pages/Correction.tsx
+++ client/src/pages/Correction.tsx
@@ -136,8 +136,6 @@ export default function Correction() {
     });
   };
 
-  const hasClockIn  = !!(newClockInDate  && newClockInTimeStr);
-  const hasClockOut = !!(newClockOutDate && newClockOutTimeStr);
   const isFormValid =
     selectedEmployeeId &&
     selectedRecordId &&
